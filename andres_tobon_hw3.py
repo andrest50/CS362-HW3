5befae3a488a9eb299da85e58658f975f7cb7f9a
Andres <andrest2455@gmail.com>
2021-01-27 15:29:28 -0800

    Added flowchart image and added comments to the updated program.

--- andres_tobon_hw3.py
+++ andres_tobon_hw3.py
@@ -10,8 +10,11 @@ To run the script, use the following command:
 def main():
     valid = False
 
+    """ Continuously receive input until valid input or force quit """
     while(valid == False):
         yearString = input("Input a year: ")
+
+        """ Try converting input into int and catch exceptions below """
         try:
             year = int(yearString)
             if(year < 1):
@@ -20,6 +23,7 @@ def main():
         except ValueError:
             print("Invalid year. Input a positive integer number.")
 
+    """ Calculate if the year is a leap year and print result """
     if(year % 4 == 0):
         if(year % 100 == 0):
             if(year % 400 == 0):
